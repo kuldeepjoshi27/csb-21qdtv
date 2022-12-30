2edb1a1c8664321ac5649272bb72077ead72e453
Code Sandbox <hello@codesandbox.io>
2022-12-30 14:04:17 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.ac67cd9444c1bf8625fbecebaa2cfd51.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9330cdfa7c2ad7e09f5ee1c85ba64a77",
+    "url": "https://kuldeepjoshi27.github.io/csb-21qdtv/index.html"
+  },
+  {
+    "revision": "ba74d873453c33644f10",
+    "url": "https://kuldeepjoshi27.github.io/csb-21qdtv/static/js/2.4d9ed812.chunk.js"
+  },
+  {
+    "revision": "f3d36aa324d71f6af21f",
+    "url": "https://kuldeepjoshi27.github.io/csb-21qdtv/static/js/main.81bce340.chunk.js"
+  },
+  {
+    "revision": "9c49b673fbad6ea84ad3",
+    "url": "https://kuldeepjoshi27.github.io/csb-21qdtv/static/js/runtime-main.f95991bf.js"
+  }
+]);
